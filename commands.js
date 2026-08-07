893e4934fa33cfbdff7a8a73182a19524292554b
Jeff Z Stride <jeffstride@hotmail.com>
2026-08-07 22:49:18 +0000

    restore top-level commands.js

---
+++ commands.js
@@ -0,0 +1,9 @@
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+export const ALL_COMMANDS = [TEST_COMMAND];
